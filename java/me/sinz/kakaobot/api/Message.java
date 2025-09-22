65a886db901d11b51d673cfa7d9c72796db3cf53
SinZ <osk0121@naver.com>
2025-09-22 23:59:05 +0900

    Create Message.java

---
+++ java/me/sinz/kakaobot/api/Message.java
@@ -0,0 +1,68 @@
+package me.sinz.kakaobot.api;
+
+import org.mozilla.javascript.ScriptableObject;
+
+import me.sinz.kakaobot.Replier;
+
+public class Message extends ScriptableObject {
+
+    private ScriptableObject scope;
+
+    String room, content, packageName, channelId, logId;
+    Author author;
+    boolean isGroupChat;
+    Replier replier;
+    Image image;
+    int userId;
+
+    public Message(ScriptableObject scope, String room, String msg, Author author, boolean isGroupChat, Replier replier,
+                   Image ImageDB, String packageName, int userId, String chatId, String chatLogId) {
+        this.scope = scope;
+        this.room = room;
+        this.content = msg;
+        this.author = author;
+        this.isGroupChat = isGroupChat;
+        this.image = new Image(scope);
+        this.packageName = packageName;
+        this.userId = userId;
+        this.channelId = chatId;
+        this.logId = chatLogId;
+        this.replier = replier;
+
+        initParameters();
+    }
+
+    private void initParameters() {
+        putConst("room", this, this.room);
+        putConst("content", this, this.content);
+        putConst("author", this, this.author);
+        putConst("isGroupChat", this, this.isGroupChat);
+        putConst("image", this, this.image);
+        putConst("packageName", this, this.packageName);
+        putConst("channelId", this, this.channelId);
+        putConst("logId", this, this.logId);
+        putConst("isMultiChat", this, this.userId != 0);
+
+        putConst("isDebugRoom", this, false);
+
+
+        defineFunctionProperties(new String[]{
+                "reply",
+                "markAsRead"
+        }, Message.class, 0);
+    }
+
+    public void reply(String msg) {
+        this.replier.reply(msg);
+    }
+
+    public void markAsRead() {
+        this.replier.markAsRead();
+    }
+
+    @Override
+    public String getClassName() {
+        return "Message";
+    }
+
+}
